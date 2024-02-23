398c5c9145d882c6a25e95bc2c196fe707874823
PC <pompeu@ua.pt>
2024-02-23 15:11:11 +0000

    fix bug where last sequence in buffer was discarded

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ void processString(const std::string& s) {
 }
 
 int main(int argc,char* argv[]) {
-    std::string line;
     std::ifstream inputFile("teste.txt");
 
     if(!inputFile.is_open()) {
@@ -19,14 +18,28 @@ int main(int argc,char* argv[]) {
 
     char buffer[bufferSize + 1];
     inputFile.read(buffer,bufferSize);
-    buffer[inputFile.gcount()] = '\0';
     int bufferLen = 0;
     while((bufferLen = inputFile.gcount())) {
-        std::string s(buffer);
-
-        processString(s);
-        inputFile.read(buffer,bufferSize);
         buffer[bufferLen] = '\0';
+        std::string s(buffer);
+        processString(s);
+
+        //mete o ponteiro para tras para garantir que a ultima sequencia é processada
+        //EXEMPLO
+        //ficheiro: exemplomaisexemplo
+        //k: 3
+        //bufferSize: 5
+        //buffer: exemp
+        //primeira sequencia: exe => adivinha m
+        //segunda sequencia: xem => adivinha p
+        //nao faz mais nenhuma sequencia nesse buffer porque chega ao fim do buffer, sequencia seria emp
+        //nesta altura o ponteiro do ficheiro esta no l
+        //metendo para tras k vezes, ou seja, -3
+        //o ponteiro passa a estar no e de emp
+        //logo o buffer fica emplo em vez de lpoma
+        //e assim a sequencia emp é processada em vez de ser descartada
+        inputFile.seekg(-k,inputFile.cur);
+        inputFile.read(buffer,bufferSize);
     }
 
     inputFile.close();
